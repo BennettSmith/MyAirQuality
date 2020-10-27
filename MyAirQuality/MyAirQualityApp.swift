689ecd83b0501469a6630cb175e51ac8d707c176
Bennett Smith <bennettsmith@mac.com>
2020-10-27 11:13:56 -0700

    Initial Commit

---
+++ MyAirQuality/MyAirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAirQualityApp.swift
+//  MyAirQuality
+//
+//  Created by Bennett Smith on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct MyAirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
